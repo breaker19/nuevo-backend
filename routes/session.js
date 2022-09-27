6c23e6d76f1314f183517ca50b686faa1506090f
breaker19 <sebakarp26@gmail.com>
2022-09-27 20:15:30 -0300

    nueva entrega con twilio

--- routes/session.js
+++ routes/session.js
@@ -4,7 +4,12 @@ const session = require("express-session");
 const rutas = Router();
 const path = require("path");
 const app = express();
+const twilio = require('twilio');
 
+const accountSid = 'ACaff6db7044ba385a30a0925eba462274'
+const authToken = '817ffc5ff4611f791cec0885a9027143'
+
+const client = twilio(accountSid, authToken)
 app.set("views", path.join(path.dirname(""), "./views"));
 
 rutas.route("/");
@@ -15,13 +20,36 @@ rutas.get("/login", (req, res) => {
 });
 
 
-rutas.post("/login", (req, res) => {
+rutas.post("/login", (req, res, next) => {
   const registro = {
     username: req.body.username,
     password: req.body.password,
   };
+
   res.send(`Bienvenido: ${registro.username}`);
   console.log(registro.username);
+
+  try {
+    //async
+    if(registro){
+   const message =  client.messages.create({
+   
+      body: `Bienvenido: ${registro.username}`,
+      from: '+19377447141',
+      to: '+541133398915',
+     
+
+     
+   })}
+    else{
+        console.log('No se envio el mensaje');
+    }
+   
+} catch (error) {
+   console.log(error)
+}
 });
+
+
 
 module.exports = rutas;
